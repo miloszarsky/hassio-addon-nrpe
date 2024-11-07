1263784880ba095defd32644b31bbe01cd2fb3c3
Milos Zarsky <milos.zarsky@gmail.com>
2024-11-07 23:03:26 +0100

    version3

--- nagios-nrpe/rootfs/run.sh
+++ nagios-nrpe/rootfs/run.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bashio
+bashio::log.info "Starting nrpe server..."
 mkdir -p /var/run/nagios /etc/nagios/certs /usr/lib/nagios/extra
 [ -e "/var/run/nagios/nrpe.pid" ] && rm "/var/run/nagios/nrpe.pid"
 ALLOWED_HOSTS=$(bashio::config 'allowed_hosts')
-[ -z $(bashio::config 'allowed_hosts') ] && sed -i "s/allowed_hosts=0.0.0.0/allowed_hosts=$allowed_hosts/g" /etc/nagios/nrpe.cfg
+[ -z $(bashio::config 'allowed_hosts') ] && sed -i "s/allowed_hosts=0.0.0.0/allowed_hosts=$ALLOWED_HOSTS/g" /etc/nagios/nrpe.cfg
 /usr/bin/nrpe -c /etc/nagios/nrpe.cfg -f -n
