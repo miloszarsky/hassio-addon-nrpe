a2b7b6f5da1e1c590dcb4c3ebbdeb317d446dc97
Milos Zarsky <milos.zarsky@gmail.com>
2024-11-07 22:57:18 +0100

    add variable

--- nagios-nrpe/rootfs/run.sh
+++ nagios-nrpe/rootfs/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 mkdir -p /var/run/nagios /etc/nagios/certs /usr/lib/nagios/extra
 [ -e "/var/run/nagios/nrpe.pid" ] && rm "/var/run/nagios/nrpe.pid"
+ALLOWED_HOSTS=$(bashio::config 'allowed_hosts')
+[ -z $(bashio::config 'allowed_hosts') ] && sed -i "s/allowed_hosts=0.0.0.0/allowed_hosts=$allowed_hosts/g" /etc/nagios/nrpe.cfg
 /usr/bin/nrpe -c /etc/nagios/nrpe.cfg -f -n
